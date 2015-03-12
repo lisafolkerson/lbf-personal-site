b326a65a7c163e5ce14482839ad0484e109c9bb7
Lisa Folkerson <lisafolkerson@gmail.com>
2015-03-11 20:22:59 -0400

    progress on theme

--- wp-content/themes/theme-hackeryou/page-home.php
+++ wp-content/themes/theme-hackeryou/page-home.php
@@ -8,21 +8,24 @@ get_header();  ?>
 
 <div class="main clearfix">
   <div class="container clearfix">
+      <div class="aboutMe">
+        <?php // Start the loop ?>
+        <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-    <?php // Start the loop ?>
-    <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-
-<!--       <h2><?php the_title(); ?></h2> -->
+    <!--       <h2><?php the_title(); ?></h2> -->
       <?php the_content(); ?>
-
+      </div> <!-- end .aboutMe -->
+      
       <div class="portfolioPieces" id="portfolio">
 
         <?php //going to pull in the latest 4 blog posts ?>
         <?php $latestPosts = new WP_Query(array(
         		'post_type' => 'portfolio', //only want portfolio posts
-        		'posts_per_page' => 4
+        		'posts_per_page' => 6
         )) ?>
+
         <?php if ($latestPosts->have_posts()) while($latestPosts->have_posts()) : $latestPosts->the_post(); ?>
+
         	<div class="featuredPost">
         		<h2><?php the_title() ?></h2>
         	</div> <!--end featured post-->
